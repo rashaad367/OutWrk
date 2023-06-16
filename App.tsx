c224fbfd80c1174352d3325e08e259cd64baacc2
Rashaad Jones <rashaadjones367@gmail.com>
2023-06-16 17:43:20 -0400

    added navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+import { NavigationContainer, NavigationProp } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./src/views/Login";
+import SignUp from "./src/views/SignUp";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/views/Login.tsx
@@ -0,0 +1,48 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+import { NavigationContainer, NavigationProp } from "@react-navigation/native";
+
+type LoginProps = {
+  navigation: NavigationProp<Record<string, object>>;
+};
+
+const Login = ({ navigation }: LoginProps) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>This is the login page. Cool.</Text>
+      <Button
+        title="Go to sign up page."
+        onPress={() => navigation.navigate({ name: "SignUp", params: {} })}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 96,
+  },
+  grayBlock: {
+    width: 129,
+    height: 6,
+    backgroundColor: "#d9d9d9",
+  },
+  wrapperBlock: {
+    width: 340,
+    height: 491,
+    backgroundColor: "#d9d9d9",
+    marginTop: 20,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Login;
